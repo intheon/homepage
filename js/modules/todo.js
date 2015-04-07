6fc7693d524037167f69d5c3f57d29e74502c84f
intheon <allobon@gmail.com>
2015-04-07 17:09:01 +0100

    seems to still work
    not for long.... :3

--- js/modules/todo.js
+++ js/modules/todo.js
@@ -6,6 +6,32 @@ $(document).ready(function(){
 
 function loadTodoList()
 {
-	$("#todo").append("<div class='inline field'><label>Enter something new</label><input placeholder='What?' type='text'></div>")
+
+	$("#todo").append("<div class='ui form'>\
+		<div class='inline fields'>\
+			<div class='field'>\
+				<label>New todo?</label>\
+				<input placeholder='wat?' type='text'>\
+			</div>\
+			<div class='field'>\
+				<div class='ui labeled button'>Submit</div>\
+			</div>\
+		</div>\
+	</div>");
+
+	/*
+	$("#todo").append("<div class='ui form'>
+		<div class='inline fields'>\
+		<div class='ui labeled input field' id='emptyToDo'>\
+			<label class='ui label'>New?</label>\
+			<input class='ui field' placeholder='wat?' type='text' id='emptyToDoField'>\
+		</div>\
+		<div class='field'>\
+			<div class='ui labeled button' id='submitButton'>Submit</div>\
+		</div>\
+				</div>\
+		</div>");
+
+*/
 }
 
